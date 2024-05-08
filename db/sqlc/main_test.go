43f7374a012e2aab13614714fa27bd2a48ddc23e
tam.nguyen <tam.nguyen@gmail.com>
2024-05-08 14:29:36 +0700

    milestone: complete part 14

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/nguyenhothanhtam0709/go-simplebank/utils"
 )
 
 var testQueries *Queries
@@ -19,7 +15,13 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+
+	config, err := utils.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
