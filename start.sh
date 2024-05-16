eab064d4dac732a89281e8c9bc4f2bdd1ff9461c
tam.nguyen <tam.nguyen@gmail.com>
2024-05-17 00:17:03 +0700

    milestone: complete part 27

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e # script exit immidiately if the command returns non-0 status
+
+echo "run db migration"
+/app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
+
+echo "start the app"
+exec "$@" # take all parameters passed to the script and run it
