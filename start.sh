2d5459a44079c188b5e0e74bf0a89944eef1ed0b
tam.nguyen <tam.nguyen@gmail.com>
2024-06-06 22:35:06 +0700

    ci: load app.env before running migration

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e # script exit immidiately if the command returns non-0 status
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
